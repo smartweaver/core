78f0fc5b3c60ec77f17a42b24770ba393fa116bc
Eric Crooks <crookse@users.noreply.github.com>
2023-10-29 01:19:49 -0400

    test: small test with protected property

---
+++ tests/standard/chains/IsolatedHandlerChain.unit.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "vitest";
+import { IsolatedHandlerChain } from "../../../src/standard/chains/IsolatedHandlerChain.ts";
+
+describe("extensibility", () => {
+    describe("chain_builder", () => {
+        test("can see the chain_builder property", () => {
+            class A extends IsolatedHandlerChain {
+                c: string[] = []
+                use(c: string) {
+                    this.c.push(c);
+                    this.chain_builder.handler({ function_name: "test", handle: () => true })
+                    return this;
+                }
+            }
+
+            const a = new A()
+                .use("1234")
+
+            expect(a.c).toStrictEqual(["1234"])
+        })
+    })
+})
